4f4c6d4db54d8211e22729596970a969344ed298
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-18 16:32:58 -0300

    Agrego uuid en el create

--- clase-02/01.repaso-metodos-http-express/server.js
+++ clase-02/01.repaso-metodos-http-express/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const uuid = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 const app = express()
 const PORT = 8080
 
 const db = [
-    { id: 1, nombre: 'PC', precio: 200},
-    { id: 2, nombre: 'Notebook', precio: 500},
-    { id: 3, nombre: 'Mouse', precio: 50},
+    { id: '1', nombre: 'PC', precio: 200},
+    { id: '2', nombre: 'Notebook', precio: 500},
+    { id: '3', nombre: 'Mouse', precio: 50},
 ]
 
 // ! Middlewares
@@ -23,22 +23,22 @@ app.use(express.json()) // Entender el json que se envíe desde el frontend
 // El backend es el que se encarga de generar el ID.
 
 app.post('/api/v1/productos', (req, res) => {
-    // 1. Vamos recibir información (objeto request (req))
+    // ! 1. Vamos recibir información (objeto request (req))
     // El objeto req -> Tiene una key llamada body (Se recibe la información que se envían desde el front en el cuerpo )
     console.log(req.body)
-    // Lógica de javascript
+    // ! Lógica de javascript
     const {nombre, precio} = req.body
     console.log(nombre)
     console.log(precio)
 
     if (!nombre || !precio) return res.status(400).json({ mensaje: 'Datos inválidos'})
     
-    const nuevoProducto = { nombre, precio, id: uuid()}
+    const nuevoProducto = { nombre, precio, id: uuidv4()}
     console.log(nuevoProducto)
-    
+    db.push(nuevoProducto)
 
-    // 2. Vamos a responder (objeto response (res))
-    res.send('POST -> CREATE') 
+    // ! 2. Vamos a responder (objeto response (res))
+    res.status(201).json(nuevoProducto)
 })
 
 
@@ -54,7 +54,8 @@ app.get('/api/v1/productos', (req, res) => {
 app.get('/api/v1/productos/:id/', (req, res) => { 
     // Dentro del request -> key llamada -> params
     console.log(req.params) // { id }
-    const id = parseInt(req.params.id)
+    //const id = parseInt(req.params.id)
+    const id = eq.params.id
     console.log(id)
     const productoBuscando = db.find(prod => prod.id === id)
     console.log(productoBuscando)
@@ -72,7 +73,17 @@ app.get('/api/v1/productos/:id/', (req, res) => {
 // ! U:UPDATE -> PUT 
 // PUT -> /productos/:id | método PUT/PATCH | http://localhost:8080/productos/1 | body: { }
 // ! D:DELETE -> DELETE
-// DELETE -> /productos/:id | método DELETE | http://localhost:8080/productos/1
+// DELETE -> /api/v1/productos/:id | método DELETE | http://localhost:8080/api/v1/productos/1
+
+app.delete('/api/v1/productos/:id', (req, res) => {
+    // ! 1. Gestiona la informaicón que llega
+    // key params es la clave donde van a estar llegando los parametros pasados por URL
+    const idEliminar = req.params.id
+    console.log(idEliminar)
+
+    // ! 2. Respondo al frontend
+    res.send('DELETE')
+})
 
 
 app.listen(PORT, () => {
