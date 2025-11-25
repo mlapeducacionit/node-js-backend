807f86ec270275ef6cf7b1375aac0b4ced39d94e
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-25 16:42:34 -0300

    Listo proyecto base

---
+++ clase-04/02.express-validator/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'dotenv/config'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+// ! Constantes y Variables
+const app = express()
+const PORT = process.env.PORT
+const FILENAME = fileURLToPath(import.meta.url)
+const DIRNAME = path.dirname(FILENAME)
+
+// ! Middlewares a nivel de aplicación
+app.use(express.static(path.join(DIRNAME, 'public')))
+app.use(express.json())
+
+// ! Rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`)
+})
