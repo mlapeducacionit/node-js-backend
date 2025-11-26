a1c2f9e7849589bcdeae0311f54c9182439d60c1
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-26 14:29:21 -0300

    Validación artesanal

--- clase-05/01.express-validator.cont/server.js
+++ clase-05/01.express-validator.cont/server.js
@@ -21,7 +21,30 @@ app.get('/', (req, res) => {
 
 app.post('/datos-contacto', (req, res) => {
     console.log(req.body)
-    res.json('OK POST')
+    const error = []
+
+    const {name, lastname, age} = req.body
+
+    if(!name) {
+      //return res.status(400).json({ error: 'El nombre es requerido'})
+      error.push({error: 'El nombre es requerido'})
+    }
+
+    if(!lastname) {
+      //return res.status(400).json({ error: 'El apellido es requerido'})
+      error.push({error: 'El apellido es requerido'})
+    }
+
+    if(!age) {
+      //return res.status(400).json({ error: 'El edad es requerido'})
+      error.push({error: 'El edad es requerido'})
+    }
+
+    if(error.length > 0) {
+      return res.status(400).json({ error })
+    }
+
+    res.status(201).json({ mensaje: 'Todo Okey', nombre: name, apellido: lastname, edad: age })
 })
 
 app.listen(PORT, () => {
