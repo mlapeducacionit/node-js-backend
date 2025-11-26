dcce26b4e4d475bf7266dfad9c356540e2100987
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-26 15:04:11 -0300

    Trabajando con express validator

--- clase-05/01.express-validator.cont/server.js
+++ clase-05/01.express-validator.cont/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import { fileURLToPath } from 'url'
 import path from 'path'
+import { check, validationResult } from 'express-validator'
 
 // ! Constantes y Variables
 const app = express()
@@ -47,10 +48,28 @@ app.post('/datos-contacto-manual', (req, res) => {
     res.status(201).json({ mensaje: 'Todo Okey', nombre: name, apellido: lastname, edad: age })
 })
 
-app.post('/datos-contacto', (req, res) => {
+// Middleware de ruta -> Express Validator -> https://express-validator.github.io/docs
+// -> https://github.com/validatorjs/validator.js
+// https://express-validator.github.io/docs/category/guides
+app.post('/datos-contacto', [
+                              check('name', 'El nombre es obligatorio y tiene que tener 3 a 10 caracteres').notEmpty().isLength({ min: 3, max: 12}).trim(1),
+                              check('lastname', 'El apellido es obligatorio').notEmpty(),
+                              check('age', 'La edad es obligatoria y tiene que ser mayor de edad. Entre 18 y 99').notEmpty().isInt({ min: 18, max: 99}),
+                              check('email', 'El correo es inválido').notEmpty().isEmail(),
+                              check('password', 'La contraseña es obligatoria y debe tener por lo menos un caracteres mayusucula, minuscula y numeros').notEmpty().matches(/^[A-Za-z0-9]{6,}$/),
+                              (req, res, next) => {
+                                console.log(req) // <----- las marcas que dejan los check
+                                const errores = validationResult(req) // Se encarga de extrar lo que fueron generando los check
+                                console.log(errores)
+                                if ( !errores.isEmpty() ) {
+                                  return res.status(400).json( errores )
+                                }
+                                next()
+                              }
+                            ], (req, res) => {
     console.log(req.body)
    
-    res.status(201).json({ mensaje: 'Todo Okey' })
+    res.status(201).json({ mensaje: 'Todo Okey', data: req.body })
 })
 
 app.listen(PORT, () => {
