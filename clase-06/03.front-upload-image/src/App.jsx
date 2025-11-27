a2bfa8dbbc9a357ecc0d7e9e15c6dc4c2fcd6bf0
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-27 14:40:55 -0300

    Listo captura de datos del formulario

--- clase-06/03.front-upload-image/src/App.jsx
+++ clase-06/03.front-upload-image/src/App.jsx
@@ -1,22 +1,42 @@
+
+
 const App = () => {
 
 
+  const envioFormulario = (e) => {
+    e.preventDefault()
+    console.log('Enviando la data...')
+
+    console.log(e.target)
+    const formu = e.target
+
+    const formData  = new FormData(formu)
+
+    console.log(formData)
+
+    formData.forEach((valor, clave) => {
+      console.log(clave)
+      console.log(valor)
+    })
+
+    
+
+  }
 
   return (
     <div className="container mt-4">
       <h1 className="display-6 text-success">Formulario de subida de imagenes</h1>
-      <form className="border border-successs rounded p-4">
+      <form onSubmit={envioFormulario} className="border border-success rounded p-4">
 
-        <div class="mb-3">
-          <label for="subida-archivo" className="form-label">Subir archivo: </label>
-          <input type="file" className="form-control" id="subida-archivo" placeholder="Elija un archivo" />
+        <div className="mb-3">
+          <label htmlFor="subida-archivo" className="form-label">Subir archivo: </label>
+          <input type="file" name="archivo" className="form-control" id="subida-archivo" placeholder="Elija un archivo" />
         </div>
 
         <button className="btn btn-success">Enviar</button>
       </form>
     </div>
   )
-
 
 }
 
