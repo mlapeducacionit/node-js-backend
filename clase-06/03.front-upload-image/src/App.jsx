f85cb2f403d6c931fd3222a75dccb3ad8b111c88
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-11-27 14:30:03 -0300

    Listo formulario de subida

--- clase-06/03.front-upload-image/src/App.jsx
+++ clase-06/03.front-upload-image/src/App.jsx
@@ -1,7 +1,23 @@
 const App = () => {
+
+
+
   return (
-    <h1>Hola mundo!</h1>
+    <div className="container mt-4">
+      <h1 className="display-6 text-success">Formulario de subida de imagenes</h1>
+      <form className="border border-successs rounded p-4">
+
+        <div class="mb-3">
+          <label for="subida-archivo" className="form-label">Subir archivo: </label>
+          <input type="file" className="form-control" id="subida-archivo" placeholder="Elija un archivo" />
+        </div>
+
+        <button className="btn btn-success">Enviar</button>
+      </form>
+    </div>
   )
+
+
 }
 
 export default App
