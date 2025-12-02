5614d25f1bdfff3e5b63d3847a18ae677033a752
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-12-02 14:27:54 -0300

    Agregamos las rutas de usuario en nuestra app

---
+++ clase-08/01.backend-integrador.cont/server.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import 'dotenv/config'
+import logger from './configs/logger.js'
+import handleConnection from './utils/handle-connection.js'
+
+// ! Variables / Contantes
+const app = express()
+const PORT = process.env.PORT || 8088
+
+// ! Configuraciones
+console.log(process.env)
+
+
+// ! Middlewares
+
+// ! Rutas
+app.get('/', (req, res) => {
+
+    if (req.query.data) {
+        logger.info('Se recibió la data')
+        logger.info(req.query.data)
+        res.send(req.query.data)
+    } else {
+        logger.error('No se recibió la data')
+        res.status(400).send('Nada llego.')
+    }
+    
+})
+
+// CRUD Usuarios
+app.get('/user/login', (req, res) => {
+  res.send('login get')
+})
+app.post('/user/login', (req, res) => {
+  res.send('login post')
+})
+app.get('/user/register', (req, res) => {
+  res.send('register get')
+})
+app.post('/user/register', (req, res) => {
+  res.send('register post')
+})
+app.put('/user/edit/:id', (req, res) => {
+  res.send('edit')
+})
+app.delete('/user/delete/:id', (req, res) => {
+  res.send('delete')
+})
+
+
+
+
+
+// ! Arranque del servidor
+app.listen(PORT, (err) => {
+  //if ( err ) console.log(err)
+  if ( err ) {
+    logger.error('No se pudo levantar el servidor')
+    logger.error(err)
+  }    
+  //console.log(`El servidor funciona correctamente en: http://localhost:${PORT}`)
+  logger.info(`El servidor funciona correctamente en: http://localhost:${PORT}`)
+  const uri = 'mongodb://localhost:27017/db_integrador'
+  handleConnection(uri)
+})
+
