9b2a2c6914497de5c0598160752df8a7fce79734
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-12-03 16:53:29 -0300

    creamos un endpoint para enviar cookies

--- clase-09/02.autenticacion-sesiones/server.js
+++ clase-09/02.autenticacion-sesiones/server.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+
 const app = express()
 const PORT = process.env.PORT || 8088
+
+// Middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   const nombre = 'Maxi'
   res.send(`Hello ${nombre}`)
+})
+
+app.get('/set-cookie', (req, res) => {
+
+  res.cookie('nombre', 'Maxi')
+  res.cookie('modo', 'dark')
+  res.send('Te envío una cookie')
 })
 
 app.listen(PORT, () => {
