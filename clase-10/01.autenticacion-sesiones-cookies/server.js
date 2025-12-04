5b38802b0757d0ddd17d590495e9e10356e55e47
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-12-04 14:49:36 -0300

    Empezamos con sesiones

--- clase-10/01.autenticacion-sesiones-cookies/server.js
+++ clase-10/01.autenticacion-sesiones-cookies/server.js
@@ -1,12 +1,30 @@
 import express from 'express'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
+import session from 'express-session'
 
 const app = express()
 const PORT = process.env.PORT || 8088
 
 // Middleware
+// ! Cookies
 app.use(cookieParser())
+// ! Sesiones
+
+// * secret -> cadena de caracteres que se va a usar para generar sesiones ->  Esto va dentro de una variable de entorno -> secret es una semilla que me va permitir generar un sid único para mi servidor
+// * resave -> false (recomendado) -> Permite indica si se va a estar guardando cada vez que se haga una petición.
+// * saveUninitialized -> false (recomendado) -> Ni bien crea sesión, si crea vacía no la guardo.
+// * cookie. Controla que la cookie sea segura.
+// * store: Permite especificar donde se van a guardar las sesiones creadas. Por defecto si no le coloco, guarda en memoria.
+
+app.use(session(
+  {
+    secret: 'sarasa-tudobom-1234567-%%$$',
+    resave: false,
+    saveUninitialized: false,
+    /* cookie: { secure: true} */
+  }
+))
 
 app.get('/', (req, res) => {
   const nombre = 'Maxi'
@@ -24,6 +42,33 @@ app.get('/get-cookies', (req, res) => {
   console.log(req.cookies) // 
   res.send('Cookies recibidas...')
 })
+
+/* -------------------------------------------- */
+/* SESIONES------------------------------------ */
+/* -------------------------------------------- */
+let contadorSinSesion = 0
+
+app.get('/sin-sesion', (req, res) => {
+  /* res.send('Sin Sesion') */
+  res.json({ contador: ++contadorSinSesion })
+})
+
+app.get('/con-sesion', (req, res) => {
+
+  console.log(req.session)
+  // Variable de sesión
+  //req.session.contador = 1
+
+  if (req.session.contador) {
+    req.session.contador++
+    res.json({ mensaje: `Usted ha visitado el sitio: ${req.session.contador}` })
+  } else {
+    req.session.contador = 1
+    res.json({ mensaje: 'Bienvenido!'})
+  }
+
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`)
