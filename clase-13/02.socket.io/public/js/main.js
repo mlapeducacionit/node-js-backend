b1a87c2658f55097a26cf6154a0071f8f72d5935
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-12-12 16:12:52 -0300

    incorporamos la librería socket.io en nuestro proyecto

--- clase-13/02.socket.io/public/js/main.js
+++ clase-13/02.socket.io/public/js/main.js
@@ -1,1 +1,2 @@
 console.log('Funcioando...')
+console.log(io) // http://localhost:8080/socket.io/socket.io.js
--- clase-13/02.socket.io/server.js
+++ clase-13/02.socket.io/server.js
@@ -2,12 +2,19 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import variablesEntorno from './constants/variables-entorno.js'
+import { createServer } from 'http' // Api node http
+import { Server } from 'socket.io'
 
 // ! Variables | Constantes
 const app = express()
 const PORT = 8080
 const FILENAME = fileURLToPath(import.meta.url)
 const DIRNAME = path.dirname(FILENAME)
+
+// ! Configuraciones
+// * Agrego la librería socket.io
+const server = createServer(app)
+const io = new Server(server)
 
 // ! Middleware
 app.use(express.static(path.join(DIRNAME, variablesEntorno.public_directory)))
@@ -16,6 +23,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Servidor funcioanndo en http://localhost:${PORT}`)
 })
