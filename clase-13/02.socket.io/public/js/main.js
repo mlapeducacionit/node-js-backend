faaaaba7219ad029549ee9b01fb23c821a8156d5
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-12-12 16:54:26 -0300

    listo interfaz chat

--- clase-13/02.socket.io/public/js/main.js
+++ clase-13/02.socket.io/public/js/main.js
@@ -1,2 +1,23 @@
 console.log('Funcioando...')
 console.log(io) // http://localhost:8080/socket.io/socket.io.js
+
+const socket = io.connect() // Este se conecta al receptor
+
+socket.on('nombre', (nombre) => {
+    console.log(nombre)
+})
+
+socket.on('objeto-producto', (objeto) => {
+    console.log(objeto)
+})
+
+socket.on('array-numeros', (arrayNumero) => {
+    console.log(arrayNumero)
+    arrayNumero.forEach(element => {
+        console.log(element)
+    })
+})
+
+socket.on('mensajes', (mensajes) => {
+    console.log(mensajes)
+})
--- clase-13/02.socket.io/server.js
+++ clase-13/02.socket.io/server.js
@@ -10,6 +10,13 @@ const app = express()
 const PORT = 8080
 const FILENAME = fileURLToPath(import.meta.url)
 const DIRNAME = path.dirname(FILENAME)
+let clientesConectados = 0
+const mensajes = [
+    { usuario: 'Pedro', mensaje: 'Hola! que tal!'},
+    { usuario: 'Alfredo', mensaje: 'Muy bien y vos?'},
+    { usuario: 'Natalia', mensaje: 'Genial' },
+    { usuario: 'Laura', mensaje: 'Todo feten feten' }
+]
 
 // ! Configuraciones
 // * Agrego la librería socket.io
@@ -18,6 +25,38 @@ const io = new Server(server)
 
 // ! Middleware
 app.use(express.static(path.join(DIRNAME, variablesEntorno.public_directory)))
+
+
+io.on('connection', (socket) => {
+    console.log('Un cliente se ha conectado', socket.id) // es como un dni pero podría no ser único
+    clientesConectados++
+    console.log(clientesConectados)
+    // socket.id -> único por conexión
+    // socket.id -> No es un identificador estable de usuario (no)
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id)
+        clientesConectados--
+        console.log(clientesConectados)
+    })
+
+
+    // Emitir mensajes desde el servidor al cliente
+    socket.emit('nombre', 'Maximiliano')
+
+    socket.emit('objeto-producto', {
+        nombre: 'PC',
+        categoria: 'Informatica',
+        precio: 123
+    })
+
+    socket.emit('array-numeros', [4, 3, 2, 5, 7, 8, 12, 34])
+
+    /* --------------------- APP Chat en vivo */
+
+    socket.emit('mensajes', mensajes)
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
